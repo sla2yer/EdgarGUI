623c107600995fbe09f855e01f9e4c34bad7ac2f
Jack Rubio <rubiojack123@gmail.com>
2022-09-11 22:38:53 -0400

    removed print lines

--- Edgar_filingviewer_handler.py
+++ Edgar_filingviewer_handler.py
@@ -478,11 +478,6 @@ class FilingViewerHandler:
             for line in range(len(self.result_lines[0])):
                 if len(self.result_lines[0]) < 1:
                     print("ERROR efh481 resline len==0")
-                print("-------------------------------------------------")
-                print(line)
-                print(len(self.result_lines[0]))
-                print(len(self.result_lines))
-                print(len(self.result_lines[1]))
                 if pcla not in self.result_lines[0][line - num_popped]:
                     self.result_lines[1].append(self.result_lines[0].pop(line - num_popped))
                     num_popped = num_popped + 1
